412c1addb80845f925934821b875fe2ba95c050b
heatray <heatrayws@gmail.com>
2024-04-13 15:48:03 +0300

    Add Loader module

---
+++ source/loader.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+
+const char* DataPath = "Data2";
+
+DWORD XString__XString = 0x402285;
+DWORD XString__FreeRep = 0x402280;
+DWORD XomApp__GetInstance = 0x413444;
+
+void __declspec(naked) InitOptionalPathsCodeCave()
+{
+    __asm {
+        jne     FreeRepIcons
+        call    XString__FreeRep
+    FreeRepIcons:
+        push    DataPath
+        lea     ecx, dword ptr ss:[esp+0x4]
+        call    XString__XString
+        call    XomApp__GetInstance
+        mov     ecx, dword ptr ds:[eax]
+        lea     edx, dword ptr ss:[esp]
+        push    edx
+        push    eax
+        call    dword ptr ds:[ecx+0xC]
+        add     ecx, 0xFFFFFFFA
+        dec     word ptr ds:[ecx]
+        cmp     word ptr ds:[ecx], 0
+        jne     FreeRepData2
+        call    XString__FreeRep
+    FreeRepData2:
+        pop     ecx
+        retn
+    }
+}
+
+void Init()
+{
+    // WormsXApp::InitOptionalPaths
+    injector::MakeJMP(0x4AB345, InitOptionalPathsCodeCave);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
+{
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        if (strcmp((char*)0x7766A8, "Worms 4: Mayhem!") == 0)
+        {
+            Init();
+        }
+    }
+    return TRUE;
+}
