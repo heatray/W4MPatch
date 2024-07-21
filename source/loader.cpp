171c0d47b30bc439fc5098b788cfd154a5c4d09c
heatray <heatrayws@gmail.com>
2024-07-21 20:10:48 +0300

    Fix Loader

--- source/loader.cpp
+++ source/loader.cpp
@@ -2,12 +2,18 @@
 
 const char* DataPath = "Data2";
 
+DWORD XString__FreeRep = 0x402280;
 DWORD XString__XString = 0x402285;
 DWORD XomHelp__XomAppBase__GetInstance = 0x413444;
+DWORD InitOptionalPathsCodeCaveExit = 0x4AB34C;
 
 void __declspec(naked) InitOptionalPathsCodeCave()
 {
     __asm {
+        cmp     word ptr ds : [ecx], 0
+        jne     FreeRepJump
+        call    XString__FreeRep
+    FreeRepJump:
         push    DataPath
         lea     ecx, dword ptr ss : [esp + 0x4]
         call    XString__XString
@@ -17,15 +23,14 @@ void __declspec(naked) InitOptionalPathsCodeCave()
         push    edx
         push    eax
         call    dword ptr ds : [ecx + 0xC]
-        pop     ecx
-        retn
+        jmp     InitOptionalPathsCodeCaveExit
     }
 }
 
 void Init()
 {
     // WormsXApp::InitOptionalPaths
-    injector::MakeJMP(0x4AB34C, InitOptionalPathsCodeCave);
+    injector::MakeJMP(0x4AB341, InitOptionalPathsCodeCave);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
