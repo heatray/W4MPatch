e2225f5546722bb149c6ed80f17c09b14a264bd8
heatray <heatrayws@gmail.com>
2025-12-16 20:43:08 +0300

    Fix premake script

--- premake5.lua
+++ premake5.lua
@@ -20,10 +20,9 @@ workspace "W4MAnniversaryPatch"
    includedirs { "source/injector" }
    includedirs { "source/inireader" }
 
-   date = os.outputof("date /t")
-   yy = tonumber(string.sub(date, 9, 10))
-   mm = tonumber(string.sub(date, 4, 5))
-   dd = 0 -- tonumber(string.sub(date, 1, 2))
+   yy = os.date("%y")
+   mm = os.date("%m")
+   dd = 0 -- os.date("%d")
    build = 0
 
    defines {
@@ -52,10 +51,8 @@ workspace "W4MAnniversaryPatch"
 
 project "W4M.Patch"
    files { "source/patch.cpp" }
-
    defines { "rsc_FileDescription=\"Patch for Worms 4 Mayhem\"" }
 
 project "W4M.Loader"
    files { "source/loader.cpp" }
-
    defines { "rsc_FileDescription=\"Loader for Worms 4 Mayhem\"" }
